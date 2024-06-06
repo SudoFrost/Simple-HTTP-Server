430692e18f870c368795582d4493c5701f14b941
Ali Talebi <sudofrost@gmail.com>
2024-06-06 20:53:25 +0330

    Implement simple TCP srever

--- server.go
+++ server.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	fmt.Println("Server started on port 8080")
+
+	conn, err := listener.Accept()
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Client connected")
+
+	conn.Write([]byte("Hello World"))
+	conn.Close()
 }
